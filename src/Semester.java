91eaaa2128970ce75fd262b9b19646de6eac8779
thebigby01 <md.a_rahmanasif_x86-64@protonmail.com>
2024-05-22 16:06:24 +0600

    Added semister add/remove/check login in Semister.java class

--- src/Semester.java
+++ src/Semester.java
@@ -1,64 +1,48 @@
-import java.util.*;
-public class Semester extends Main {
-    int semester;
-   ArrayList <Course> cou ;
-    ArrayList <Course> getCou()
-    {
-        return cou;
+import java.io.File;
+
+public class Semester {
+    public boolean addSemister(String semester){
+        //In case data folder does not exists it will create one.
+        File dataFolder = new File("data/");
+        if(!dataFolder.mkdir()  && !dataFolder.exists())
+            return false;
+        File semesterFolder = new File("data/"+semester);
+        if(semesterFolder.mkdir())
+            return true;
+        else
+            return false;
     }
 
-    public Semester(int semester)
-    {
-     this.semester=semester;
-       this.cou=new ArrayList<>();
+    public boolean isSemisterAvailable(String semester){
+        File semesterFolder = new File("data/"+semester);
+        if(semesterFolder.exists() && semesterFolder.isDirectory())
+            return true;
+        else
+            return false;
     }
-    public void addcourse(ArrayList<Course> course)
-    {
 
-
-        System.out.println("Write course name");
-        Scanner scan = new Scanner(System.in);
-        String nm = scan.nextLine();
-        course.add(new Course(nm));
-        System.out.println("0 for go back \n1 for adding another course");
-        int as=scan.nextInt();
-        if(as==0)showsem(sem);
-        else if (as==1)addcourse(cou);
-        else System.out.println("invalid input , try again");
-
+    public boolean removeSemister(String semester){
+        File semesterFolder = new File("data/"+semester);
+        if(deleteDirectory(semesterFolder))
+            return true;
+        else
+            return false;
     }
-    public void showcourse(ArrayList<Course> course)
-    {
-        System.out.println("ok");
-        for(Course cr: course)
-        {
-            System.out.println(cr.name+"\n");
-        }
-        System.out.println("1 - add a course");
-        System.out.println("0- go back");
-        System.out.println("2- access a course");
-        Scanner scan = new Scanner(System.in);
-        int as = scan.nextInt();
-        if(as==0)showsem(sem);
-        else if (as==1)addcourse(cou);
-        else if (as==2) {
-
-            System.out.println("Enter course name");
-           String sc= scan.nextLine();
-            String cname = scan.nextLine();
-            int ind;
-            for(Course cr: course )
-            {
-                if(cname==cr.name) { ind= course.indexOf(cr);break;}
+    //a method to 
+    public static boolean deleteDirectory(File directory) {
+        if (directory.isDirectory()) {
+            // get all the directories in the current file
+            File[] files = directory.listFiles();
+            if (files != null) {
+                // recursivly delete each folder and file
+                for (File file : files) {
+                    deleteDirectory(file);
+                }
             }
-            System.out.println("1-add a material\n2");
-
         }
-        else System.out.println("invalid input");
 
+        // Delete the directory or file
+        return directory.delete();
     }
-
-
-
 
 }
